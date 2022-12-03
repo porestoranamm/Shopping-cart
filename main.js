da99d3a10729f8f02dc7dd76792888b9d773061f
Nikita Kuznetsov <livisback@yandex.ru>
2022-12-03 15:11:02 +0600

    add shopItemsData

--- main.js
+++ main.js
@@ -1,23 +1,33 @@
 let shop = document.getElementById("shop")
 
+let shopItemsData = [{
+    id: "items1",
+    name: "Casual Shirt" , 
+    price: 45,
+    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing",
+    img: "images/img-1.jpg"
+},{
+    id: "items2",
+    name: "Office shirt" , 
+    price: 100,
+    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing",
+    img: "images/img-2.jpg"
+},{
+    id: "items3",
+    name: "T Shirt" , 
+    price: 25,
+    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing",
+    img: "images/img-3.jpg"
+},{
+    id: "items4",
+    name: "Mens Suit" , 
+    price: 300,
+    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing",
+    img: "images/img-4.jpg"
+}]
+
 let generateShop = () => {
-    return (shop.innerHTML=`
-    <div class="item">
-            <img width="220" src="images/img-1.jpg" alt="">
-            <div class="details">
-                <h3>Casual Shirt</h3>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing.</p>
-                <div class="price-quantity"></div>
-                <h2>$ 45</h2>
-                <div class="buttons">
-                    <i class="bi bi-dash-lg"></i>
-                    <div class="quantity">0</div>
-                    <i class="bi bi-plus-lg"></i>
-                </div>
-            </div>
-        </div>
-    </div>
-    `)
+    return (shop.innerHTML= );
 };
 
 generateShop();
