e842ab024bc6de4f581ca707557e2edf392db781
Nikita Kuznetsov <livisback@yandex.ru>
2022-12-07 13:43:16 +0600

    create code generateShop

--- main.js
+++ main.js
@@ -27,7 +27,28 @@ let shopItemsData = [{
 }]
 
 let generateShop = () => {
-    return (shop.innerHTML= shopItemsData.map((x)=>{}));
+    return (shop.innerHTML= shopItemsData
+        .map((x)=>{
+            let{id, name, price, desc, img} = x
+        return `
+        <div class="item">
+            <img width="220" src="${x.img}" alt="">
+            <div class="details">
+                <h3>${name}</h3>
+                <p>${desc}</p>
+                <div class="price-quantity"></div>
+                <h2>${price}</h2>
+                <div class="buttons">
+                    <i class="bi bi-dash-lg"></i>
+                    <div class="quantity">0</div>
+                    <i class="bi bi-plus-lg"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+    `
+    })
+    .join(""));
 };
 
 generateShop();
