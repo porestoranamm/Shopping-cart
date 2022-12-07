e77ce683b84cea2d85fb6476be9b780a49956961
Nikita Kuznetsov <livisback@yandex.ru>
2022-12-07 15:02:46 +0600

    create code basket

--- main.js
+++ main.js
@@ -77,12 +77,7 @@ let decrement =(id) => {
     let selectedItem = id;
     let search = basket.find((x)=> x.id === selectedItem.id);
 
-    if (search === undefined) {
-    basket.push({
-        id: selectedItem.id,
-        item: 1,
-    });
-}
+    if (search.item === 0) return;
     else {
         search.item -= 1;
     }
