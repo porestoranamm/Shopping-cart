662e4f6119d877e9c8b04bf592c7418d7652c0d0
Nikita Kuznetsov <livisback@yandex.ru>
2022-12-08 12:57:46 +0600

    create shopItems

--- main.js
+++ main.js
@@ -1,34 +1,32 @@
 let shop = document.getElementById("shop")
 
+let basket = JSON.parse(localStorage.getItem("data")) || [];
+
 let shopItemsData = [{
     id: "items1",
     name: "Casual Shirt" , 
     price: 45,
     desc: "Lorem ipsum dolor sit, amet consectetur adipisicing",
-    img: "images/img-1.jpg"
+    img: "images/img-1.jpg",
 },{
     id: "items2",
     name: "Office shirt" , 
     price: 100,
     desc: "Lorem ipsum dolor sit, amet consectetur adipisicing",
-    img: "images/img-2.jpg"
+    img: "images/img-2.jpg",
 },{
     id: "items3",
     name: "T Shirt" , 
     price: 25,
     desc: "Lorem ipsum dolor sit, amet consectetur adipisicing",
-    img: "images/img-3.jpg"
+    img: "images/img-3.jpg",
 },{
     id: "items4",
     name: "Mens Suit" , 
     price: 300,
     desc: "Lorem ipsum dolor sit, amet consectetur adipisicing",
-    img: "images/img-4.jpg"
-}]
-
-let basket = [{
-   
-}]; 
+    img: "images/img-4.jpg",
+}];
 
 let generateShop = () => {
     return (shop.innerHTML= shopItemsData
